6dcb979b1a7bb26a5c2d61edaf021444a55aeee6
AkaChou <mc123@vip.qq.com>
2026-06-25 18:34:25 +0800

    feat(game): prefer battlefield beans in runtime bridge

--- src/main/java/com/aionemu/gameserver/lifecycle/GameBattlefieldRuntimeBridge.java
+++ src/main/java/com/aionemu/gameserver/lifecycle/GameBattlefieldRuntimeBridge.java
@@ -11,12 +11,69 @@ import com.aionemu.gameserver.services.instance.IronWallWarfrontService;
 import com.aionemu.gameserver.services.instance.KamarBattlefieldService;
 import com.aionemu.gameserver.services.instance.SuspiciousOphidanBridgeService;
 import com.aionemu.gameserver.utils.Util;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 @Lazy
 public class GameBattlefieldRuntimeBridge {
+
+    private ObjectProvider<KamarBattlefieldService> kamarBattlefieldServiceProvider;
+    private ObjectProvider<EngulfedOphidanBridgeService> engulfedOphidanBridgeServiceProvider;
+    private ObjectProvider<SuspiciousOphidanBridgeService> suspiciousOphidanBridgeServiceProvider;
+    private ObjectProvider<IronWallWarfrontService> ironWallWarfrontServiceProvider;
+    private ObjectProvider<IdgelDomeService> idgelDomeServiceProvider;
+    private ObjectProvider<IdgelDomeLandmarkService> idgelDomeLandmarkServiceProvider;
+    private ObjectProvider<HallOfTenacityService> hallOfTenacityServiceProvider;
+    private ObjectProvider<GrandArenaTrainingCampService> grandArenaTrainingCampServiceProvider;
+    private ObjectProvider<IDRunService> idRunServiceProvider;
+
+    @Autowired(required = false)
+    void setKamarBattlefieldServiceProvider(ObjectProvider<KamarBattlefieldService> kamarBattlefieldServiceProvider) {
+        this.kamarBattlefieldServiceProvider = kamarBattlefieldServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setEngulfedOphidanBridgeServiceProvider(ObjectProvider<EngulfedOphidanBridgeService> engulfedOphidanBridgeServiceProvider) {
+        this.engulfedOphidanBridgeServiceProvider = engulfedOphidanBridgeServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setSuspiciousOphidanBridgeServiceProvider(ObjectProvider<SuspiciousOphidanBridgeService> suspiciousOphidanBridgeServiceProvider) {
+        this.suspiciousOphidanBridgeServiceProvider = suspiciousOphidanBridgeServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setIronWallWarfrontServiceProvider(ObjectProvider<IronWallWarfrontService> ironWallWarfrontServiceProvider) {
+        this.ironWallWarfrontServiceProvider = ironWallWarfrontServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setIdgelDomeServiceProvider(ObjectProvider<IdgelDomeService> idgelDomeServiceProvider) {
+        this.idgelDomeServiceProvider = idgelDomeServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setIdgelDomeLandmarkServiceProvider(ObjectProvider<IdgelDomeLandmarkService> idgelDomeLandmarkServiceProvider) {
+        this.idgelDomeLandmarkServiceProvider = idgelDomeLandmarkServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setHallOfTenacityServiceProvider(ObjectProvider<HallOfTenacityService> hallOfTenacityServiceProvider) {
+        this.hallOfTenacityServiceProvider = hallOfTenacityServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setGrandArenaTrainingCampServiceProvider(ObjectProvider<GrandArenaTrainingCampService> grandArenaTrainingCampServiceProvider) {
+        this.grandArenaTrainingCampServiceProvider = grandArenaTrainingCampServiceProvider;
+    }
+
+    @Autowired(required = false)
+    void setIdRunServiceProvider(ObjectProvider<IDRunService> idRunServiceProvider) {
+        this.idRunServiceProvider = idRunServiceProvider;
+    }
 
     public void printBattlefieldSection() {
         Util.printSection(" *** Battlefield *** ");
@@ -27,38 +84,65 @@ public class GameBattlefieldRuntimeBridge {
     }
 
     public KamarBattlefieldService kamarBattlefieldService() {
-        return KamarBattlefieldService.getInstance();
+        if (kamarBattlefieldServiceProvider == null) {
+            return KamarBattlefieldService.getInstance();
+        }
+        return kamarBattlefieldServiceProvider.getIfAvailable(KamarBattlefieldService::getInstance);
     }
 
     public EngulfedOphidanBridgeService engulfedOphidanBridgeService() {
-        return EngulfedOphidanBridgeService.getInstance();
+        if (engulfedOphidanBridgeServiceProvider == null) {
+            return EngulfedOphidanBridgeService.getInstance();
+        }
+        return engulfedOphidanBridgeServiceProvider.getIfAvailable(EngulfedOphidanBridgeService::getInstance);
     }
 
     public SuspiciousOphidanBridgeService suspiciousOphidanBridgeService() {
-        return SuspiciousOphidanBridgeService.getInstance();
+        if (suspiciousOphidanBridgeServiceProvider == null) {
+            return SuspiciousOphidanBridgeService.getInstance();
+        }
+        return suspiciousOphidanBridgeServiceProvider.getIfAvailable(SuspiciousOphidanBridgeService::getInstance);
     }
 
     public IronWallWarfrontService ironWallWarfrontService() {
-        return IronWallWarfrontService.getInstance();
+        if (ironWallWarfrontServiceProvider == null) {
+            return IronWallWarfrontService.getInstance();
+        }
+        return ironWallWarfrontServiceProvider.getIfAvailable(IronWallWarfrontService::getInstance);
     }
 
     public IdgelDomeService idgelDomeService() {
-        return IdgelDomeService.getInstance();
+        if (idgelDomeServiceProvider == null) {
+            return IdgelDomeService.getInstance();
+        }
+        return idgelDomeServiceProvider.getIfAvailable(IdgelDomeService::getInstance);
     }
 
     public IdgelDomeLandmarkService idgelDomeLandmarkService() {
-        return IdgelDomeLandmarkService.getInstance();
+        if (idgelDomeLandmarkServiceProvider == null) {
+            return IdgelDomeLandmarkService.getInstance();
+        }
+        return idgelDomeLandmarkServiceProvider.getIfAvailable(IdgelDomeLandmarkService::getInstance);
     }
 
     public HallOfTenacityService hallOfTenacityService() {
-        return HallOfTenacityService.getInstance();
+        if (hallOfTenacityServiceProvider == null) {
+            return HallOfTenacityService.getInstance();
+        }
+        return hallOfTenacityServiceProvider.getIfAvailable(HallOfTenacityService::getInstance);
     }
 
     public GrandArenaTrainingCampService grandArenaTrainingCampService() {
-        return GrandArenaTrainingCampService.getInstance();
+        if (grandArenaTrainingCampServiceProvider == null) {
+            return GrandArenaTrainingCampService.getInstance();
+        }
+        return grandArenaTrainingCampServiceProvider.getIfAvailable(GrandArenaTrainingCampService::getInstance);
     }
 
     public IDRunService idRunService() {
-        return IDRunService.getInstance();
+        if (idRunServiceProvider == null) {
+            return IDRunService.getInstance();
+        }
+        return idRunServiceProvider.getIfAvailable(IDRunService::getInstance);
     }
 }
