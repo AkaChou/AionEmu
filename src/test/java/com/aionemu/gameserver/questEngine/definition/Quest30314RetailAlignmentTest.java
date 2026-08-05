9757a54dc398fe59fff21ef9b395fd3377c0e40c
AkaChou <mc123@vip.qq.com>
2026-08-06 00:12:14 +0800

    fix(quest): enforce quest 30314 certification turn-in

---
+++ src/test/java/com/aionemu/gameserver/questEngine/definition/Quest30314RetailAlignmentTest.java
@@ -0,0 +1,29 @@
+package com.aionemu.gameserver.questEngine.definition;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Quest30314RetailAlignmentTest {
+	@Test
+	void setRewardRequiresAndConsumesTheCertificationItems() throws Exception {
+		Path path = Path.of("src/main/resources/aion/data/static_data/quest_definition/quests/30314.xml");
+		try (InputStream input = Files.newInputStream(path)) {
+			QuestDefinition definition = QuestDefinitionXmlCompiler.compile(input).definition();
+			QuestTransition setReward = definition.transitions().stream()
+				.filter(transition -> "started".equals(transition.sourceNode()))
+				.filter(transition -> "reward".equals(transition.targetNode()))
+				.filter(transition -> transition.event() instanceof QuestEvent.TalkToNpc talk
+					&& Integer.valueOf(10255).equals(talk.dialogId()))
+				.findFirst().orElseThrow();
+			assertEquals(List.of(new QuestCondition.HasItem(186000098, 100)), setReward.conditions());
+			assertEquals(List.of(new QuestAction.RemoveItem(186000098, QuestAction.RemoveItem.ALL)),
+				setReward.actions());
+		}
+	}
+}
